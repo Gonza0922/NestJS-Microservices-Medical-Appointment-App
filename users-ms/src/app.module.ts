9c2c2396bd2a2d9e07f5f0545ca2d2cb31079303
Gonzalo <gonzalo.nieto0922@gmail.com>
2024-06-12 23:57:49 -0300

    feat: mongoDB created with its schema in the users microservice

--- users-ms/src/app.module.ts
+++ users-ms/src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { User, UserSchema } from './users/schemas/user.schema';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.URL_DB, {
+      dbName: 'users',
+    }),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
