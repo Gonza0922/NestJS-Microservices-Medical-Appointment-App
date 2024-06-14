90f3d473071fb9260b2b098834ac96855d2c66e4
Gonzalo <gonzalo.nieto0922@gmail.com>
2024-06-14 16:51:32 -0300

    feat: added validations with class-validator on the gateway

--- gateway/src/appointments/dto/appointments.dto.ts
+++ gateway/src/appointments/dto/appointments.dto.ts
@@ -1,12 +1,25 @@
 import { PartialType } from '@nestjs/mapped-types';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  MinLength,
+} from 'class-validator';
 
 export class CreateAppointmentDto {
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
   patient_ID: number;
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
   doctorName: string;
-  appointmentDate: Date;
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(4)
   reason: string;
 }
 
-export class UpdateAppointmentDto extends PartialType(CreateAppointmentDto) {
-  appointment_ID: string;
-}
+export class UpdateAppointmentDto extends CreateAppointmentDto {}
--- gateway/src/users/dto/users.dto.ts
+++ gateway/src/users/dto/users.dto.ts
@@ -1,10 +1,39 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Matches,
+  MinLength,
+} from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
   name: string;
+  @IsEmail()
+  @IsString()
+  @IsNotEmpty()
   email: string;
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
+  @Matches(/^(?=.*[A-Z])(?=.*\d).+$/, {
+    message: 'password must contain at least one uppercase letter and a number',
+  })
   password: string;
 }
 
 export class UpdateUserDto {
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
   name: string;
+  @IsOptional()
+  @IsEmail()
+  @IsString()
+  @IsNotEmpty()
   email: string;
 }
