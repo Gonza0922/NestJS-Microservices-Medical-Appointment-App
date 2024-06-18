d6bd92d21d7d12a6b45c2e0ff1de19abc8ba6696
Gonzalo <gonzalo.nieto0922@gmail.com>
2024-06-17 21:54:26 -0300

    feat: auth endpoints created on gateway

--- gateway/src/auth/auth.controller.ts
+++ gateway/src/auth/auth.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LoginUserDto, RegisterUserDto } from './dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+  @Post('/register')
+  registerUserEndpoint(@Body() registerUser: RegisterUserDto) {
+    return this.authService.registerUser(registerUser);
+  }
+
+  @Get('/login')
+  loginUserEndpoint(@Body() loginUser: LoginUserDto) {
+    return this.authService.loginUser(loginUser);
+  }
+
+  @Get('/logout')
+  logoutUserEndpoint() {
+    return this.authService.logoutUser();
+  }
 }
--- gateway/src/auth/auth.service.ts
+++ gateway/src/auth/auth.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { LoginUserDto, RegisterUserDto } from './dto/auth.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(@Inject('AUTH') private readonly authClient: ClientProxy) {}
+
+  registerUser(createUser: RegisterUserDto) {
+    return this.authClient.send({ cmd: 'registerUser' }, createUser);
+  }
+
+  loginUser(loginUser: LoginUserDto) {
+    return this.authClient.send({ cmd: 'loginUser' }, loginUser);
+  }
+
+  logoutUser() {
+    return this.authClient.send({ cmd: 'logoutUser' }, {});
+  }
+}
