e755815a39f3805072d069b905c2a79ca74cecac
Gonzalo <gonzalo.nieto0922@gmail.com>
2024-12-05 13:18:36 -0300

    tests: testing findAll function of users.controller.ts of users-ms

--- users-ms/test/users/users.controller.spec.ts
+++ users-ms/test/users/users.controller.spec.ts
@@ -1,9 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getModelToken } from '@nestjs/mongoose';
-import { User } from './../../src/users/schemas/user.schema';
-import { UsersModule } from './../../src/users/users.module';
-import { UsersController } from './../../src/users/users.controller';
-import { UsersService } from './../../src/users/users.service';
+import { User } from '../../src/users/schemas/user.schema';
+import { UsersModule } from '../../src/users/users.module';
+import { UsersController } from '../../src/users/users.controller';
+import { UsersService } from '../../src/users/users.service';
+import { Types } from 'mongoose';
 
 describe('UserController', () => {
   let controller: UsersController;
@@ -22,22 +23,31 @@ describe('UserController', () => {
   });
 
   const userExample = {
-    name: 'exampleadp',
+    _id: new Types.ObjectId(),
+    name: 'example',
     email: 'example@gmail.com',
     password: 'example123',
   };
 
+  const documentExample = {
+    ...userExample,
+    __v: 0,
+    toJSON: jest.fn().mockReturnValue(userExample),
+  };
+
+  const mockUserModel = {
+    create: jest.fn().mockReturnValue(documentExample),
+  };
+
   describe('findAllUsers', () => {
     it('must return an object with users array and pagination', async () => {
-      jest.spyOn(service, 'findAll').mockImplementation(() =>
-        Promise.resolve({
-          data: [userExample],
-          pagination: { currentPage: 1, totalPages: 1, totalItems: 1 },
-        } as unknown as Promise<any>),
-      );
+      jest.spyOn(service, 'findAll').mockResolvedValue({
+        data: [await mockUserModel.create()],
+        pagination: { page: 1, limit: 1, totalPage: 1 },
+      });
       const result = await controller.findAll({});
       expect(result.data).toHaveLength(1);
-      expect(result.data[0]).toEqual(expect.objectContaining(userExample));
+      expect(result.data[0]).toEqual(expect.objectContaining(documentExample));
       expect(service.findAll).toHaveBeenCalledTimes(1);
     });
   });
