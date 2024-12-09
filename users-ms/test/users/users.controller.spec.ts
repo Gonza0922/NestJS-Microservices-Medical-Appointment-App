69d7a875dcdd8ee9cdf718a57152170a2b338f48
Gonzalo <gonzalo.nieto0922@gmail.com>
2024-12-09 16:36:25 -0300

    tests: testing findOneUser and updateUser function of users.controller.ts of users-ms

--- users-ms/test/users/users.controller.spec.ts
+++ users-ms/test/users/users.controller.spec.ts
@@ -1,25 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getModelToken } from '@nestjs/mongoose';
-import { User } from '../../src/users/schemas/user.schema';
+import { User, UserDocument } from '../../src/users/schemas/user.schema';
 import { UsersModule } from '../../src/users/users.module';
 import { UsersController } from '../../src/users/users.controller';
-import { UsersService } from '../../src/users/users.service';
-import { Types } from 'mongoose';
+import { Model, Types } from 'mongoose';
+import { UpdateUserDto } from 'src/users/dto/users.dto';
 
 describe('UserController', () => {
   let controller: UsersController;
-  let service: UsersService;
+  let userModel: Model<UserDocument>;
 
   beforeEach(async () => {
+    userModel = {
+      findById: jest.fn(),
+      findByIdAndUpdate: jest.fn(),
+    } as any;
+
     const module: TestingModule = await Test.createTestingModule({
       imports: [UsersModule],
     })
       .overrideProvider(getModelToken(User.name))
-      .useValue(jest.fn())
+      .useValue(userModel)
       .compile();
 
     controller = module.get<UsersController>(UsersController);
-    service = module.get<UsersService>(UsersService);
   });
 
   const userExample = {
@@ -39,16 +43,39 @@ describe('UserController', () => {
     create: jest.fn().mockReturnValue(documentExample),
   };
 
-  describe('findAllUsers', () => {
-    it('must return an object with users array and pagination', async () => {
-      jest.spyOn(service, 'findAll').mockResolvedValue({
-        data: [await mockUserModel.create()],
-        pagination: { page: 1, limit: 1, totalPage: 1 },
-      });
-      const result = await controller.findAll({});
-      expect(result.data).toHaveLength(1);
-      expect(result.data[0]).toEqual(expect.objectContaining(documentExample));
-      expect(service.findAll).toHaveBeenCalledTimes(1);
+  describe('findOneUser', () => {
+    it('must return an object with user data', async () => {
+      jest
+        .spyOn(userModel, 'findById')
+        .mockResolvedValue(await mockUserModel.create());
+      const result = await controller.findOne(userExample._id.toString());
+      expect(result).toEqual(expect.objectContaining(documentExample));
+      expect(userModel.findById).toHaveBeenCalledWith(
+        userExample._id.toString(),
+      );
+      expect(userModel.findById).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  const updateUser: UpdateUserDto = {
+    user_ID: userExample._id.toString(),
+    name: userExample.name,
+    email: userExample.email,
+  };
+
+  describe('updateUser', () => {
+    it('must return an object with user data updated', async () => {
+      jest
+        .spyOn(userModel, 'findByIdAndUpdate')
+        .mockResolvedValue(await mockUserModel.create());
+      const result = await controller.update(updateUser);
+      expect(result).toEqual(expect.objectContaining(documentExample));
+      expect(userModel.findByIdAndUpdate).toHaveBeenCalledWith(
+        updateUser.user_ID,
+        updateUser,
+        { new: true },
+      );
+      expect(userModel.findByIdAndUpdate).toHaveBeenCalledTimes(1);
     });
   });
 });
